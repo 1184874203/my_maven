c27ec535f4eec04e145ee5becf70ca0983f8fbe5
jlgl <1184874203@qq.com>
2021-07-08 11:03:48 +0800

    syh第二次开发

--- src/main/java/one.java
+++ src/main/java/one.java
@@ -2,5 +2,6 @@ public class one {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println("syh第一次开发");
+        System.out.println("syh第二次开发");
     }
 }
